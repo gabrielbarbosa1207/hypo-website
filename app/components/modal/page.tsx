9f7ae00a36b3f4655839e7685f93730cd2c7c65f
gabrielbarbosa1207 <gabrielbarbosasa2@gmail.com>
2024-04-09 16:38:51 -0300

    all

--- app/components/modal/page.tsx
+++ app/components/modal/page.tsx
@@ -87,9 +87,86 @@ const SimulationModal = () => {
       <input type="text" name="state" placeholder="State" value={state} onChange={(e) => setState(e.target.value)} />
       <input type="number" name="amountRequested" placeholder="Amount Requested" value={amountRequested} onChange={(e) => setAmountRequested(e.target.value)} />
       <input type="number" name="propertyPrice" placeholder="Property Price" value={propertyPrice} onChange={(e) => setPropertyPrice(e.target.value)} />
-      <input type="text" name="corrective" placeholder="Corrective" value={corrective} onChange={(e) => setCorrective(e.target.value)} />
-      <input type="text" name="index" placeholder="Index" value={index} onChange={(e) => setIndex(e.target.value)} />
-      <input type="text" name="amortization" placeholder="Amortization" value={amortization} onChange={(e) => setAmortization(e.target.value)} />
+
+      <div>
+        <input
+          type="radio"
+          id="post"
+          name="corrective"
+          value="Pós-fixado"
+          checked={corrective === "Pós-fixado"}
+          onChange={(e) => setCorrective(e.target.value)}
+        />
+        <label htmlFor="post">Pós-fixado</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="pre"
+          name="corrective"
+          value="Prefixado"
+          checked={corrective === "Prefixado"}
+          onChange={(e) => setCorrective(e.target.value)}
+        />
+        <label htmlFor="pre">Prefixado</label>
+      </div>
+
+
+      {/* *******************************index***************************  */}
+
+
+      <div>
+        <input
+          type="radio"
+          id="cdi"
+          name="index"
+          value="CDI"
+          checked={index === "CDI"}
+          onChange={(e) => setIndex(e.target.value)}
+        />
+        <label htmlFor="cdi">CDI</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="ipca"
+          name="index"
+          value="IPCA"
+          checked={index === "IPCA"}
+          onChange={(e) => setIndex(e.target.value)}
+        />
+        <label htmlFor="ipca">IPCA</label>
+      </div>
+
+      {/* *******************************index***************************  */}
+
+      <div>
+        <input
+          type="radio"
+          id="sac"
+          name="amortization"
+          value="SAC"
+          checked={amortization === "SAC"}
+          onChange={(e) => setAmortization(e.target.value)}
+        />
+        <label htmlFor="cdi">SAC</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="price"
+          name="amortization"
+          value="Preço"
+          checked={amortization === "Preço"}
+          onChange={(e) => setAmortization(e.target.value)}
+        />
+        <label htmlFor="price">Preço</label>
+      </div>
+
+      
       <input type="number" name="paymentPeriod" placeholder="Payment Period" value={paymentPeriod} onChange={(e) => setPaymentPeriod(e.target.value)} />
       <input type="number" name="gracePeriod" placeholder="Grace Period" value={gracePeriod} onChange={(e) => setGracePeriod(e.target.value)} />
 
